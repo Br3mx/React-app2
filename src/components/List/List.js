904fd2013b9f37c475df4ac718a60e9668f1e7e1
Przemysław Bulanda <devprzemas@gmail.com>
2023-12-09 00:00:21 +0100

    Commit for help

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,21 +2,15 @@ import styles from './List.module.scss'
 import Column from '../Column/Column.js';
 
 const List = () => {
-<<<<<<< HEAD
     const columns = [
-        { id: 1, title: 'Books', icon: 'book'},
-        { id: 2, title: 'Movies', icon: 'film'},
-        { id: 3, title: 'Games', icon: 'gamepad'},
+        { id: 1, title: 'Books', icon: 'book' },
+      { id: 2, title: 'Movies', icon: 'film' },
+      { id: 3, title: 'Games', icon: 'gamepad' }
     ];
-    return (
-    <div className={styles.list}>
-        <header className={styles.header}>
-            <h2 className={styles.title}>THINGS DO<span>SOON!</span></h2>
-        </header>
-            <p className={styles.description}>Interesting things i want to check out!</p>
-            <section className={styles.columns}>
-                {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon}></Column>)}
-=======
+    setTimeout(() => {
+        columns.push({ id: 4, title: 'Test column'});
+        console.log(columns)
+      }, 2000);
     return (
     <div className={styles.list}>
         <header className={styles.header}>
@@ -24,14 +18,13 @@ const List = () => {
         </header>
             <p className={styles.description}>Interesting things i want to check out!</p>
             <section className={styles.columns}>
-                <Column icon="book" text="Books"/>
-                <Column icon="gamepad" text="Movies"/>
-                <Column icon="film" text="Games"/>
->>>>>>> d4da3cc5a80d87f98e5f70f121f677a44b11385a
+                {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon}></Column>)}
+
             </section>
     </div>
         
     );
+    
 }
 
 export default List;
