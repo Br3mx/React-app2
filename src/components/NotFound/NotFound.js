7d8792c8e73a253daf335fe15f54f83a477ba0cd
Przemysław Bulanda <devprzemas@gmail.com>
2024-01-13 23:38:45 +0100

    Add pages, and page not found

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,9 @@
+import styles from './NotFound.module.scss'
+
+const NotFound = () => {
+    return (
+        <h1 className={styles.notFound}>404 NOT FOUND <br></br>:(</h1>
+    )
+}
+
+export default NotFound;
